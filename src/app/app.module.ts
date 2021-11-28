12cb19428f97d138e90dd1a67df9fcd16b9e8b67
Dominik Rudzki <dominikrudzki2000@gmail.com>
2021-11-28 16:51:55 +0100

    create FiltersComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,42 @@ import { AppComponent } from './app.component';
 import { NavComponent } from './shared/components/nav/nav.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { JobsPageComponent } from './jobs-page/jobs-page.component';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatCardModule } from '@angular/material/card';
+import { FiltersComponent } from './jobs-page/filters/filters.component';
+
+const materialModules = [
+	MatToolbarModule,
+	MatIconModule,
+	MatButtonModule,
+	MatSidenavModule,
+	MatListModule,
+	MatMenuModule,
+	MatCardModule,
+];
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavComponent,
-    FooterComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		NavComponent,
+		FooterComponent,
+		JobsPageComponent,
+  FiltersComponent,
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		BrowserAnimationsModule,
+		materialModules,
+	],
+	providers: [],
+	bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
