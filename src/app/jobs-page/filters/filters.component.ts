7e47515b714de109827a48f7829e43b86e924611
Dominik Rudzki <dominikrudzki2000@gmail.com>
2021-11-29 22:33:46 +0100

    create categories list

--- src/app/jobs-page/filters/filters.component.ts
+++ src/app/jobs-page/filters/filters.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-filters',
-  templateUrl: './filters.component.html',
-  styleUrls: ['./filters.component.scss']
+	selector: 'app-filters',
+	templateUrl: './filters.component.html',
+	styleUrls: ['./filters.component.scss'],
 })
 export class FiltersComponent implements OnInit {
+	categories: string[] = [
+		'C++',
+		'C#',
+		'JavaScript',
+		'PHP',
+		'Java',
+		'Python',
+		'.Net',
+		'Ruby',
+	];
 
-  constructor() { }
+	panelOpenState = false;
 
-  ngOnInit(): void {
-  }
+	constructor() {}
 
+	ngOnInit(): void {}
 }
