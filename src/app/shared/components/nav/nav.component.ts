dd37c70d34de8d3be0c2fd4d679c5e21973b5d2e
Dominik Rudzki <dominikrudzki2000@gmail.com>
2021-12-01 17:16:58 +0100

    add dark mode logic

--- src/app/shared/components/nav/nav.component.ts
+++ src/app/shared/components/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { CookieService } from 'src/app/core/services/cookie.service';
 
 @Component({
 	selector: 'app-nav',
@@ -7,12 +9,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 	isOpen: boolean = false;
+	darkTheme = new BehaviorSubject<boolean>(false);
 
-	constructor() {}
+	constructor(private cookieService: CookieService) {
+		const darkThemeValue = cookieService.getCookie('darkTheme');
+
+		if (darkThemeValue === '') {
+			cookieService.setCookie('darkTheme', 'false');
+		} else {
+			JSON.parse(darkThemeValue.toLowerCase());
+			this.darkTheme.next(JSON.parse(darkThemeValue.toLowerCase()));
+		}
+
+		const body = document.body.classList;
+		this.darkTheme.subscribe((obs) => {
+			obs ? body.add('darkTheme') : body.remove('darkTheme');
+			cookieService.setCookie('darkTheme', obs.toString());
+		});
+	}
 
 	ngOnInit(): void {}
 
 	toggleNav(): void {
 		this.isOpen = !this.isOpen;
+	}
+
+	toggleTheme(): void {
+		this.darkTheme.next(!this.darkTheme.value);
 	}
 }
