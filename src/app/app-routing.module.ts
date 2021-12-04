b67dd1eb78c5960264b3a4b204ec80877e3abf82
Dominik Rudzki <dominikrudzki2000@gmail.com>
2021-12-04 23:53:31 +0100

    update routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { JobsPageComponent } from './jobs-page/jobs-page.component';
 
-const routes: Routes = [{ path: '', component: JobsPageComponent }];
+const routes: Routes = [
+	{ path: '', component: JobsPageComponent },
+	{ path: 'matches', component: JobsPageComponent },
+	// TODO: add logos and jobs pages
+];
 
 @NgModule({
 	imports: [RouterModule.forRoot(routes)],
