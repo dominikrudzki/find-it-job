f20857462226534337e1428cf3833d368414fde2
Dominik Rudzki <dominikrudzki2000@gmail.com>
2021-12-07 16:23:27 +0100

    create filterPreferences actions

---
+++ src/app/shared/store/actions/filterPreferences.actions.ts
@@ -0,0 +1,7 @@
+import { createAction, props } from '@ngrx/store';
+
+export const getPreferences = createAction(
+	'[Filter Preferences] Get preferences'
+);
+
+// TODO: add rest preferences actions
