ea0f0ac1c95cf346d96dfcb8e75b7c03d5cc90bd
Dominik Rudzki <dominikrudzki2000@gmail.com>
2022-01-09 19:38:49 +0100

    update filter reducer, add remote, experience,  preference handle

--- src/app/shared/store/reducers/filterPreferences.reducer.ts
+++ src/app/shared/store/reducers/filterPreferences.reducer.ts
@@ -1,15 +1,51 @@
 import { createReducer, on } from '@ngrx/store';
-import { getPreferences } from '../actions/filterPreferences.actions';
-
+import {
+	addTechnology,
+	deleteTechnology,
+	getPreferences,
+	setExperience,
+	setRemote,
+} from '../actions/filterPreferences.actions';
 import { Preferences } from '../models/filterPreferences.model';
 
-export const initialFilterPreferences: Preferences = {
-	technologies: undefined,
-	remote: undefined,
+export const initialState: Preferences = {
 	experience: undefined,
+	remote: undefined,
+	technologies: [],
 };
 
 export const filterPreferencesReducer = createReducer(
-	initialFilterPreferences,
-	on(getPreferences, (state) => state)
+	initialState,
+	on(setRemote, (state, { remote }) => {
+		const stateCopy = { ...state };
+		if (remote) {
+			stateCopy.remote = JSON.parse(remote.toLowerCase());
+		} else {
+			stateCopy.remote = undefined;
+		}
+
+		state = stateCopy;
+		return state;
+	}),
+	on(setExperience, (state, { exp }) => {
+		const stateCopy = { ...state };
+		stateCopy.experience = exp;
+		state = stateCopy;
+		return state;
+	}),
+	on(getPreferences, (state) => state),
+	on(addTechnology, (state, { technology }) => {
+		const stateCopy = { ...state };
+		stateCopy.technologies = [...stateCopy.technologies!, technology];
+		state = stateCopy;
+		return state;
+	}),
+	on(deleteTechnology, (state, { technology }) => {
+		const stateCopy = { ...state };
+		stateCopy.technologies = stateCopy.technologies!.filter(
+			(val) => val !== technology
+		);
+		state = stateCopy;
+		return state;
+	})
 );
