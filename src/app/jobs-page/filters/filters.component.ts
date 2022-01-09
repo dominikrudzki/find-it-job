2ba861ff0caa1ad17aff1626a14406db037347dc
Dominik Rudzki <dominikrudzki2000@gmail.com>
2022-01-09 19:39:46 +0100

    add handle preferences methods

--- src/app/jobs-page/filters/filters.component.ts
+++ src/app/jobs-page/filters/filters.component.ts
@@ -1,4 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { MatCheckboxChange } from '@angular/material/checkbox';
+import { MatRadioChange } from '@angular/material/radio';
+import { Store } from '@ngrx/store';
+import { BehaviorSubject } from 'rxjs';
+import {
+	addTechnology,
+	deleteTechnology,
+	setExperience,
+	setRemote,
+} from 'src/app/shared/store/actions/filterPreferences.actions';
+import { Preferences } from 'src/app/shared/store/models/filterPreferences.model';
 
 @Component({
 	selector: 'app-filters',
@@ -17,9 +28,44 @@ export class FiltersComponent implements OnInit {
 		'Ruby',
 	];
 
+	showPreferences: boolean = false;
+	preferences: BehaviorSubject<Preferences> = new BehaviorSubject({});
 	panelOpenState = false;
 
-	constructor() {}
+	constructor(private store: Store<{ filterPreferences: any }>) {}
 
-	ngOnInit(): void {}
+	ngOnInit(): void {
+		this.store.select('filterPreferences').subscribe((obs) => {
+			this.preferences.next(obs);
+
+			const prefValue = this.preferences.value;
+			if (
+				prefValue.experience ||
+				prefValue.remote !== undefined ||
+				prefValue.technologies?.length !== 0
+			) {
+				this.showPreferences = true;
+			} else {
+				this.showPreferences = false;
+			}
+		});
+	}
+
+	remoteHandle(event: MatRadioChange) {
+		this.store.dispatch(setRemote({ remote: event.value }));
+	}
+
+	experienceHandle(event: MatRadioChange) {
+		this.store.dispatch(setExperience({ exp: event.value }));
+	}
+
+	categoryHandle(event: MatCheckboxChange) {
+		if (event.checked) {
+			this.store.dispatch(addTechnology({ technology: event.source.id }));
+		} else {
+			this.store.dispatch(
+				deleteTechnology({ technology: event.source.id })
+			);
+		}
+	}
 }
