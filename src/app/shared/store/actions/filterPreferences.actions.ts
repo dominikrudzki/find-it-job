614d724292bfcad1d7bb541cefe763f717843eea
Dominik Rudzki <dominikrudzki2000@gmail.com>
2022-01-09 19:39:22 +0100

    add remote and techonogy actions

--- src/app/shared/store/actions/filterPreferences.actions.ts
+++ src/app/shared/store/actions/filterPreferences.actions.ts
@@ -4,4 +4,22 @@ export const getPreferences = createAction(
 	'[Filter Preferences] Get preferences'
 );
 
-// TODO: add rest preferences actions
+export const setExperience = createAction(
+	'[Filter Preferences] Set experience',
+	props<{ exp: 'Junior' | 'Mid' | 'Senior' }>()
+);
+
+export const setRemote = createAction(
+	'[Filter Preferences] Set remote',
+	props<{ remote?: string }>()
+);
+
+export const addTechnology = createAction(
+	'[Filter Preferences] Add technology',
+	props<{ technology: string }>()
+);
+
+export const deleteTechnology = createAction(
+	'[Filter Preferences] Delete technology',
+	props<{ technology: string }>()
+);
