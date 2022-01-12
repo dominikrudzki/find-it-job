096fe9b760e381090cee276ce49cd844732edaa4
Dominik Rudzki <dominikrudzki2000@gmail.com>
2022-01-12 23:41:20 +0100

    add data display in job component

--- src/app/jobs-page/job/job.component.ts
+++ src/app/jobs-page/job/job.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Job} from "../../shared/store/models/jobList.model";
 
 @Component({
-  selector: 'app-job',
-  templateUrl: './job.component.html',
-  styleUrls: ['./job.component.scss']
+	selector: 'app-job',
+	templateUrl: './job.component.html',
+	styleUrls: ['./job.component.scss']
 })
 export class JobComponent implements OnInit {
+	@Input() job?: Job;
 
-  constructor() { }
+	constructor() {
+	}
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+	}
 
 }
