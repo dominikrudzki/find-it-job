a69106c1331dc67d7764549b6a44461696617332
Dominik Rudzki <dominikrudzki2000@gmail.com>
2022-01-12 23:32:11 +0100

    update filterPreferences.model.ts

--- src/app/shared/store/models/filterPreferences.model.ts
+++ src/app/shared/store/models/filterPreferences.model.ts
@@ -1,5 +1,6 @@
 export interface Preferences {
 	technologies?: Array<string>;
 	remote?: boolean;
+	minSalary?: number; //TODO: add logic to minSalary
 	experience?: 'Junior' | 'Mid' | 'Senior';
 }
