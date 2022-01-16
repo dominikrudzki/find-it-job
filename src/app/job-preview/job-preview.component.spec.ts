6c4c6a0a82420d1d09e302c6f6fa3ea1793ad486
Dominik Rudzki <dominikrudzki2000@gmail.com>
2022-01-16 18:46:15 +0100

    create job-preview with temporary data

--- src/app/job-preview/job-preview.component.spec.ts
+++ src/app/job-preview/job-preview.component.spec.ts
@@ -1,25 +1,24 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { JobPreviewComponent } from './job-preview.component';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {JobPreviewComponent} from './job-preview.component';
 
 describe('JobPreviewComponent', () => {
-  let component: JobPreviewComponent;
-  let fixture: ComponentFixture<JobPreviewComponent>;
+	let component: JobPreviewComponent;
+	let fixture: ComponentFixture<JobPreviewComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ JobPreviewComponent ]
-    })
-    .compileComponents();
-  });
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			declarations: [JobPreviewComponent]
+		})
+			.compileComponents();
+	});
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(JobPreviewComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+	beforeEach(() => {
+		fixture = TestBed.createComponent(JobPreviewComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
 });
--- src/app/job-preview/job-preview.component.ts
+++ src/app/job-preview/job-preview.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {max} from 'rxjs';
+import {JobDetails} from "../shared/interfaces/jobDetails.interface";
 
 @Component({
-  selector: 'app-job-preview',
-  templateUrl: './job-preview.component.html',
-  styleUrls: ['./job-preview.component.scss']
+	selector: 'app-job-preview',
+	templateUrl: './job-preview.component.html',
+	styleUrls: ['./job-preview.component.scss']
 })
 export class JobPreviewComponent implements OnInit {
+	job: JobDetails | any = {
+		salary: {min: 32000, max: 48000}
+	};
 
-  constructor() { }
+	constructor() {
 
-  ngOnInit(): void {
-  }
+	}
+
+	ngOnInit(): void {
+	}
 
 }
