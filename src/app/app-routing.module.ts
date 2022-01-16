dda0827555042a1799ea147745d9593dd2b22837
Dominik Rudzki <dominikrudzki2000@gmail.com>
2022-01-16 18:42:12 +0100

    update job-offers routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,14 +4,9 @@ import {JobPreviewComponent} from './job-preview/job-preview.component';
 import {JobsPageComponent} from './jobs-page/jobs-page.component';
 
 const routes: Routes = [
-	{
-		path: '', component: JobsPageComponent,
-		children: [
-			{path: 'offers/:link', component: JobPreviewComponent}
-		]
-	},
+	{path: '', component: JobsPageComponent,},
 	{path: 'matches', component: JobsPageComponent},
-	// TODO: add logos and jobs pages
+	{path: 'job-offers/:link', component: JobPreviewComponent}
 ];
 
 @NgModule({
