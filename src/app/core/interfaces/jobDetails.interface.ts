f800b3fa6b60ffa239b951d0d334a2dc78e45b2c
Dominik Rudzki <dominikrudzki2000@gmail.com>
2022-01-18 16:23:21 +0100

    create JobDetails interface

---
+++ src/app/core/interfaces/jobDetails.interface.ts
@@ -0,0 +1,13 @@
+export interface JobDetails {
+	id: number;
+	name: string;
+	image: string;
+	company: string;
+	remote: boolean;
+	salary?: { min: number, max: number };
+	description?: string;
+	benefits?: Array<string>;
+	requirements?: Array<string>;
+	experience?: 'Junior' | 'Mid' | 'Senior';
+	skills?: Array<{ level: number, name: string }>;
+}
