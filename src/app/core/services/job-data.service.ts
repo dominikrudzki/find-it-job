0e45552fa979c7edcf62776dd8c498f98daef8ae
Dominik Rudzki <dominikrudzki2000@gmail.com>
2022-01-23 22:53:02 +0100

    add getJob and getJobs to job-data service

--- src/app/core/services/job-data.service.ts
+++ src/app/core/services/job-data.service.ts
@@ -1,9 +1,22 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {JobDetails} from "../interfaces/jobDetails.interface";
+import {Observable} from "rxjs";
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class JobDataService {
+	private apiUrl: string = 'http://localhost:4400/api/v1'
 
-  constructor() { }
+	constructor(private http: HttpClient) {
+	}
+
+	getJob(id: number): Observable<JobDetails> {
+		return this.http.get<JobDetails>(`${this.apiUrl}/get-job/${id}`)
+	}
+
+	getJobs(): Observable<Array<JobDetails>> {
+		return this.http.get<any>(`${this.apiUrl}/get-jobs`)
+	}
 }
