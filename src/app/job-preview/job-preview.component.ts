26add2f132cce580e67bb4cb06e3d6f755c3e8f7
Dominik Rudzki <dominikrudzki2000@gmail.com>
2022-01-23 23:03:41 +0100

    update job-preview logic

--- src/app/job-preview/job-preview.component.ts
+++ src/app/job-preview/job-preview.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {JobDetails} from "../core/interfaces/jobDetails.interface";
+import {JobDataService} from '../core/services/job-data.service';
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
 	selector: 'app-job-preview',
@@ -7,27 +9,23 @@ import {JobDetails} from "../core/interfaces/jobDetails.interface";
 	styleUrls: ['./job-preview.component.scss']
 })
 export class JobPreviewComponent implements OnInit {
-	job: JobDetails = {
-		id: 3,
-		name: 'Senior Fullstack Developer',
-		image: 'https://cdn.pixabay.com/photo/2013/07/13/10/41/hat-157581_960_720.png',
-		company: 'Fullstack Company',
-		remote: true,
-		salary: {min: 32000, max: 48000},
-		description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium perspiciatis quasi ratione. Asperiores blanditiis consequatur culpa cum est ex facilis nemo, pariatur perferendis provident, quod sit! Deserunt necessitatibus nobis ratione?',
-		benefits: ['Remote work', 'International team International team International team International team International team', 'Private medical care'],
-		requirements: ['5+ years of experience in Angular', 'Good knowledge of English'],
-		skills: [{level: 5, name: 'Angular'}, {level: 1, name: '.NET'}, {level: 3, name: 'AWS'}],
-		experience: "Senior"
-	};
+	job?: JobDetails
 
-	constructor() {
+	jobDataSub = this.jobData.getJob(Number(this.route.snapshot.paramMap.get('jobId'))).subscribe(val => {
+		this.job = val
+	});
+
+	constructor(private jobData: JobDataService, private route: ActivatedRoute) {
 	}
 
 	ngOnInit(): void {
 	}
 
 	apply() {
-		//	TODO: apply using this job id
+		//	TODO: apply using this job.id
+	}
+
+	ngOnDestroy() {
+		this.jobDataSub.unsubscribe();
 	}
 }
