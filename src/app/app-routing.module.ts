34d3b3c6f959e6e68181d7411a99221610f101a3
Dominik Rudzki <dominikrudzki2000@gmail.com>
2022-02-03 00:16:11 +0100

    update routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,40 @@
-import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
-import {JobPreviewComponent} from './job-preview/job-preview.component';
-import {JobsPageComponent} from './jobs-page/jobs-page.component';
+import { NgModule } from '@angular/core'
+import { RouterModule, Routes } from '@angular/router'
+import { JobPreviewComponent } from './job-preview/job-preview.component'
+import { JobsPageComponent } from './jobs-page/jobs-page.component'
+import { PostJobComponent } from "./post-job/post-job.component"
+import { SettingsComponent } from "./settings/settings.component"
+import { AuthGuard } from "./core/guards/auth.guard"
 
 const routes: Routes = [
-	{path: '', component: JobsPageComponent,},
-	{path: 'matches', component: JobsPageComponent},
-	{path: 'job-offers/:jobId', component: JobPreviewComponent}
-];
+	{
+		path: '',
+		component: JobsPageComponent
+	},
+	// {path: 'matches', component: JobsPageComponent},
+	{
+		path: 'job-offers/:jobId',
+		component: JobPreviewComponent
+	},
+	{
+		path: 'post-job',
+		component: PostJobComponent
+	},
+	{
+		path: 'settings',
+		component: SettingsComponent,
+		runGuardsAndResolvers: 'always',
+		canActivate: [AuthGuard]
+	},
+	{
+		path: '**',
+		redirectTo: '/'
+	}
+]
 
 @NgModule({
 	imports: [RouterModule.forRoot(routes)],
-	exports: [RouterModule],
+	exports: [RouterModule]
 })
 export class AppRoutingModule {
 }
