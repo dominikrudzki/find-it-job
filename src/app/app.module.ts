d7a6ac47dba31b83b2cd03abe3af5ff05d156c86
Dominik Rudzki <dominikrudzki2000@gmail.com>
2022-02-02 16:47:11 +0100

    add MatSnackBarModule, ReactiveFormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,40 +1,42 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
 
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {NavComponent} from './shared/components/nav/nav.component';
-import {FooterComponent} from './shared/components/footer/footer.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {JobsPageComponent} from './jobs-page/jobs-page.component';
-import {FiltersComponent} from './jobs-page/filters/filters.component';
-import {LoginDialogComponent} from './shared/dialogs/login-dialog/login-dialog.component';
-import {JobComponent} from './jobs-page/job/job.component';
-import {JobPreviewComponent} from './job-preview/job-preview.component';
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { NavComponent } from './shared/components/nav/nav.component'
+import { FooterComponent } from './shared/components/footer/footer.component'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { JobsPageComponent } from './jobs-page/jobs-page.component'
+import { FiltersComponent } from './jobs-page/filters/filters.component'
+import { LoginDialogComponent } from './shared/dialogs/login-dialog/login-dialog.component'
+import { JobComponent } from './jobs-page/job/job.component'
+import { JobPreviewComponent } from './job-preview/job-preview.component'
 
-import {StoreModule} from '@ngrx/store';
-import {userInfoReducer} from './core/state/userInfo/userInfo.reducer';
-import {filterPreferencesReducer} from './core/state/filterPreferences/filterPreferences.reducer';
-import {jobListReducer} from "./core/state/jobList/jobList.reducer";
+import { StoreModule } from '@ngrx/store'
+import { userInfoReducer } from './core/state/userInfo/userInfo.reducer'
+import { filterPreferencesReducer } from './core/state/filterPreferences/filterPreferences.reducer'
+import { jobListReducer } from "./core/state/jobList/jobList.reducer"
 
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatListModule} from '@angular/material/list';
-import {MatMenuModule} from '@angular/material/menu';
-import {MatCardModule} from '@angular/material/card';
-import {MatCheckboxModule} from '@angular/material/checkbox';
-import {MatExpansionModule} from '@angular/material/expansion';
-import {MatRadioModule} from '@angular/material/radio';
-import {MatSlideToggleModule} from '@angular/material/slide-toggle';
-import {MatDialogModule} from '@angular/material/dialog';
-import {MatTabsModule} from '@angular/material/tabs';
-import {MatInputModule} from '@angular/material/input';
-import {MatSliderModule} from '@angular/material/slider';
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatIconModule } from '@angular/material/icon'
+import { MatButtonModule } from '@angular/material/button'
+import { MatSidenavModule } from '@angular/material/sidenav'
+import { MatListModule } from '@angular/material/list'
+import { MatMenuModule } from '@angular/material/menu'
+import { MatCardModule } from '@angular/material/card'
+import { MatCheckboxModule } from '@angular/material/checkbox'
+import { MatExpansionModule } from '@angular/material/expansion'
+import { MatRadioModule } from '@angular/material/radio'
+import { MatSlideToggleModule } from '@angular/material/slide-toggle'
+import { MatDialogModule } from '@angular/material/dialog'
+import { MatTabsModule } from '@angular/material/tabs'
+import { MatInputModule } from '@angular/material/input'
+import { MatSliderModule } from '@angular/material/slider'
+import { MatSnackBarModule } from '@angular/material/snack-bar'
 
-import {CurrencyPipe} from './core/pipes/currency.pipe';
-import {HttpClientModule} from "@angular/common/http";
+import { CurrencyPipe } from './core/pipes/currency.pipe'
+import { HttpClientModule } from "@angular/common/http"
+import { ReactiveFormsModule } from "@angular/forms"
 
 const materialModules = [
 	MatToolbarModule,
@@ -52,7 +54,8 @@ const materialModules = [
 	MatTabsModule,
 	MatInputModule,
 	MatSliderModule,
-];
+	MatSnackBarModule
+]
 
 @NgModule({
 	declarations: [
@@ -64,11 +67,12 @@ const materialModules = [
 		LoginDialogComponent,
 		JobComponent,
 		JobPreviewComponent,
-		CurrencyPipe,
+		CurrencyPipe
 	],
 	imports: [
 		BrowserModule,
 		AppRoutingModule,
+		ReactiveFormsModule,
 		BrowserAnimationsModule,
 		HttpClientModule,
 		materialModules,
@@ -76,10 +80,10 @@ const materialModules = [
 			userInfo: userInfoReducer,
 			filterPreferences: filterPreferencesReducer,
 			jobList: jobListReducer
-		}),
+		})
 	],
 	providers: [],
-	bootstrap: [AppComponent],
+	bootstrap: [AppComponent]
 })
 export class AppModule {
 }
