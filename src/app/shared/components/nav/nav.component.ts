18c113870b059b4482bbd9b54d7086d0ea78b622
Dominik Rudzki <dominikrudzki2000@gmail.com>
2022-02-03 00:29:45 +0100

    update nav logic

--- src/app/shared/components/nav/nav.component.ts
+++ src/app/shared/components/nav/nav.component.ts
@@ -1,56 +1,53 @@
-import { Component, OnInit } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
-import { Store } from '@ngrx/store';
-import { BehaviorSubject, Observable } from 'rxjs';
-import { CookieService } from 'src/app/core/services/cookie.service';
-import { LoginDialogComponent } from '../../dialogs/login-dialog/login-dialog.component';
+import { Component, OnInit } from '@angular/core'
+import { MatDialog } from '@angular/material/dialog'
+import { Store } from '@ngrx/store'
+import { BehaviorSubject, Observable } from 'rxjs'
+import { LoginDialogComponent } from '../../dialogs/login-dialog/login-dialog.component'
+import { UserInfo } from "../../../core/interfaces/user-info"
+import { resetUserInfo } from "../../../core/state/userInfo/userInfo.actions"
+import { LocalStorageService } from "../../../core/services/local-storage.service"
+import { Router } from '@angular/router'
 
 @Component({
 	selector: 'app-nav',
 	templateUrl: './nav.component.html',
-	styleUrls: ['./nav.component.scss'],
+	styleUrls: ['./nav.component.scss']
 })
 export class NavComponent implements OnInit {
-	$isLogged: Observable<boolean> = this.store.select('userInfo'); //TODO: change when logged
-	isOpen: boolean = false;
-	darkTheme = new BehaviorSubject<boolean>(false);
+	$isLogged: Observable<boolean> = this.store.select((state) => state.userInfo.isLogged)
+	$isEmployer: Observable<boolean | undefined> = this.store.select((state) => state.userInfo.isEmployer)
+	isOpen: boolean = false
+	darkTheme = new BehaviorSubject<boolean>(true)
 
 	constructor(
-		private readonly store: Store<{
-			userInfo: boolean;
-		}>,
-		private cookieService: CookieService,
-		public dialog: MatDialog
+		private readonly store: Store<{ userInfo: UserInfo }>,
+		public dialog: MatDialog,
+		private localStorageService: LocalStorageService,
+		private router: Router
 	) {
-		const darkThemeValue = this.cookieService.getCookie('darkTheme');
-
-		if (darkThemeValue === '') {
-			cookieService.setCookie('darkTheme', 'false');
-		} else {
-			JSON.parse(darkThemeValue.toLowerCase());
-			this.darkTheme.next(JSON.parse(darkThemeValue.toLowerCase()));
-		}
-
-		const body = document.body.classList;
-		this.darkTheme.subscribe((obs) => {
-			obs ? body.add('darkTheme') : body.remove('darkTheme');
-			cookieService.setCookie('darkTheme', obs.toString());
-		});
+		router.events.subscribe(val => this.isOpen = false)
 	}
 
-	ngOnInit(): void {}
+	ngOnInit(): void {
+	}
 
 	toggleNav(): void {
-		this.isOpen = !this.isOpen;
+		this.isOpen = !this.isOpen
 	}
 
-	openDialog(role: string): void {
+	openDialog(employer: boolean): void {
 		this.dialog.open(LoginDialogComponent, {
-			data: role,
-		});
+			data: employer
+		})
 	}
 
 	toggleTheme(): void {
-		this.darkTheme.next(!this.darkTheme.value);
+		this.darkTheme.next(!this.darkTheme.value)
+	}
+
+	logout() {
+		this.localStorageService.removeItem('accessToken')
+		this.localStorageService.removeItem('refreshToken')
+		this.store.dispatch(resetUserInfo())
 	}
 }
