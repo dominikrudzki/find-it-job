1a03378a7eeca0bc22c31fba8bdbae7d58d3f365
Dominik Rudzki <dominikrudzki2000@gmail.com>
2022-02-03 00:16:51 +0100

    create JobDataService

--- src/app/core/services/job-data.service.ts
+++ src/app/core/services/job-data.service.ts
@@ -1,22 +1,22 @@
-import {Injectable} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {JobDetails} from "../interfaces/jobDetails.interface";
-import {Observable} from "rxjs";
+import { Injectable } from '@angular/core'
+import { HttpClient } from "@angular/common/http"
+import { JobDetails } from "../interfaces/job-details"
+import { Observable } from "rxjs"
+import { environment } from "../../../environments/environment"
 
 @Injectable({
 	providedIn: 'root'
 })
 export class JobDataService {
-	private apiUrl: string = 'http://localhost:4400/api/v1'
 
 	constructor(private http: HttpClient) {
 	}
 
 	getJob(id: number): Observable<JobDetails> {
-		return this.http.get<JobDetails>(`${this.apiUrl}/get-job/${id}`)
+		return this.http.get<JobDetails>(`${environment.apiUrl}/get-job/${id}`)
 	}
 
 	getJobs(): Observable<Array<JobDetails>> {
-		return this.http.get<any>(`${this.apiUrl}/get-jobs`)
+		return this.http.get<any>(`${environment.apiUrl}/get-jobs`)
 	}
 }
