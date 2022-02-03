83360678a7006de84c22a06f9bf4128ed435022f
Dominik Rudzki <dominikrudzki2000@gmail.com>
2022-02-04 00:00:20 +0100

    create changeCompanyImage component

---
+++ src/app/settings/change-company-image/change-company-image.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-change-company-image',
+  templateUrl: './change-company-image.component.html',
+  styleUrls: ['./change-company-image.component.scss']
+})
+export class ChangeCompanyImageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
