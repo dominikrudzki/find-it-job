cc69f4df331bc3fc03d60538a6849a449ab666fc
Dominik Rudzki <dominikrudzki2000@gmail.com>
2022-02-04 00:05:14 +0100

    create userApplications component

---
+++ src/app/settings/user-applications/user-applications.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core'
+
+@Component({
+	selector: 'app-user-applications',
+	templateUrl: './user-applications.component.html',
+	styleUrls: ['./user-applications.component.scss']
+})
+export class UserApplicationsComponent implements OnInit {
+
+	constructor() {
+	}
+
+	ngOnInit(): void {
+	}
+
+	changePassword() {
+	}
+}
