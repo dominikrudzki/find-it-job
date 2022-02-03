03d7c8f8b5dab4769acd3c0628d3f7cdf6e0f77d
Dominik Rudzki <dominikrudzki2000@gmail.com>
2022-02-04 00:01:17 +0100

    update imports in filters

--- src/app/jobs-page/filters/filters.component.ts
+++ src/app/jobs-page/filters/filters.component.ts
@@ -1,63 +1,63 @@
-import {Component, OnInit} from '@angular/core';
-import {MatCheckboxChange} from '@angular/material/checkbox';
-import {MatRadioChange} from '@angular/material/radio';
-import {Store} from '@ngrx/store';
-import {BehaviorSubject} from 'rxjs';
+import { Component, OnInit } from '@angular/core'
+import { MatCheckboxChange } from '@angular/material/checkbox'
+import { MatRadioChange } from '@angular/material/radio'
+import { Store } from '@ngrx/store'
+import { BehaviorSubject } from 'rxjs'
 import {
 	addTechnology,
 	deleteTechnology,
 	setExperience,
-	setRemote,
-} from 'src/app/core/state/filterPreferences/filterPreferences.actions';
-import {Preferences} from 'src/app/core/interfaces/filterPreferences.interface';
+	setRemote
+} from 'src/app/core/state/filterPreferences/filterPreferences.actions'
+import { Preferences } from 'src/app/core/interfaces/filter-preferences'
 
 @Component({
 	selector: 'app-filters',
 	templateUrl: './filters.component.html',
-	styleUrls: ['./filters.component.scss'],
+	styleUrls: ['./filters.component.scss']
 })
 export class FiltersComponent implements OnInit {
-	categories: string[] = ['C++', 'C#', 'JavaScript', 'PHP', 'Java', 'Python', '.Net', 'Ruby'];
+	categories: string[] = ['C++', 'C#', 'JavaScript', 'PHP', 'Java', 'Python', '.Net', 'Ruby']
 
-	showPreferences: boolean = false;
-	preferences: BehaviorSubject<Preferences> = new BehaviorSubject({});
-	panelOpenState = false;
+	showPreferences: boolean = false
+	preferences: BehaviorSubject<Preferences> = new BehaviorSubject({})
+	panelOpenState = false
 
 	constructor(private store: Store<{ filterPreferences: any }>) {
 		this.store.select('filterPreferences').subscribe((obs) => {
-			this.preferences.next(obs);
+			this.preferences.next(obs)
 
-			const prefValue = this.preferences.value;
+			const prefValue = this.preferences.value
 			if (
 				prefValue.experience ||
 				prefValue.remote !== undefined ||
 				prefValue.technologies?.length !== 0
 			) {
-				this.showPreferences = true;
+				this.showPreferences = true
 			} else {
-				this.showPreferences = false;
+				this.showPreferences = false
 			}
-		});
+		})
 	}
 
 	ngOnInit(): void {
 	}
 
 	remoteHandle(event: MatRadioChange) {
-		this.store.dispatch(setRemote({remote: event.value}));
+		this.store.dispatch(setRemote({remote: event.value}))
 	}
 
 	experienceHandle(event: MatRadioChange) {
-		this.store.dispatch(setExperience({exp: event.value}));
+		this.store.dispatch(setExperience({exp: event.value}))
 	}
 
 	categoryHandle(event: MatCheckboxChange) {
 		if (event.checked) {
-			this.store.dispatch(addTechnology({technology: event.source.id}));
+			this.store.dispatch(addTechnology({technology: event.source.id}))
 		} else {
 			this.store.dispatch(
 				deleteTechnology({technology: event.source.id})
-			);
+			)
 		}
 	}
 }
