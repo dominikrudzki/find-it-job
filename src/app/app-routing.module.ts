31f25d88096e8e7731d4ee4e438ccdd861ec188c
Dominik Rudzki <dominikrudzki2000@gmail.com>
2022-02-03 23:58:12 +0100

    update routing add children routes to settings

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,16 @@ import { JobsPageComponent } from './jobs-page/jobs-page.component'
 import { PostJobComponent } from "./post-job/post-job.component"
 import { SettingsComponent } from "./settings/settings.component"
 import { AuthGuard } from "./core/guards/auth.guard"
+import { ManageJobsComponent } from "./settings/manage-jobs/manage-jobs.component"
+import { UserApplicationsComponent } from "./settings/user-applications/user-applications.component"
+import { ChangePasswordComponent } from "./settings/change-password/change-password.component"
+import { ChangeCompanyImageComponent } from "./settings/change-company-image/change-company-image.component"
 
 const routes: Routes = [
 	{
 		path: '',
 		component: JobsPageComponent
 	},
-	// {path: 'matches', component: JobsPageComponent},
 	{
 		path: 'job-offers/:jobId',
 		component: JobPreviewComponent
@@ -22,9 +25,30 @@ const routes: Routes = [
 	},
 	{
 		path: 'settings',
-		component: SettingsComponent,
 		runGuardsAndResolvers: 'always',
-		canActivate: [AuthGuard]
+		canActivate: [AuthGuard],
+		children: [
+			{
+				path: '',
+				component: SettingsComponent
+			},
+			{
+				path: 'change-password',
+				component: ChangePasswordComponent
+			},
+			{
+				path: 'my-applications',
+				component: UserApplicationsComponent
+			},
+			{
+				path: 'change-company-image',
+				component: ChangeCompanyImageComponent
+			},
+			{
+				path: 'manage-jobs',
+				component: ManageJobsComponent
+			}
+		]
 	},
 	{
 		path: '**',
