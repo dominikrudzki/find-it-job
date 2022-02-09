ca68e04901a805d90c4f58d730a9f71a411b1cca
Dominik Rudzki <dominikrudzki2000@gmail.com>
2022-02-09 16:53:44 +0100

    update change-company-image.component.html structure, styles & logic

--- src/app/settings/change-company-image/change-company-image.component.ts
+++ src/app/settings/change-company-image/change-company-image.component.ts
@@ -1,15 +1,62 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { CompanyService } from 'src/app/core/services/company.service'
+import { SnackbarService } from "../../core/services/snackbar.service"
+import { Router } from "@angular/router"
+import { Store } from "@ngrx/store"
+import { UserInfo } from "../../core/interfaces/user-info"
+import { LocalStorageService } from "../../core/services/local-storage.service"
+import { setCompanyImage } from "../../core/state/userInfo/userInfo.actions"
 
 @Component({
-  selector: 'app-change-company-image',
-  templateUrl: './change-company-image.component.html',
-  styleUrls: ['./change-company-image.component.scss']
+	selector: 'app-change-company-image',
+	templateUrl: './change-company-image.component.html',
+	styleUrls: ['./change-company-image.component.scss']
 })
 export class ChangeCompanyImageComponent implements OnInit {
+	updateBtnDisabled: boolean = true
+	inputFile?: File
+	imgSrc?: string | ArrayBuffer | null
 
-  constructor() { }
+	constructor(
+		private companyService: CompanyService,
+		private snackbarService: SnackbarService,
+		private router: Router,
+		private store: Store<{ userInfo: UserInfo }>,
+		private localStorageService: LocalStorageService
+	) {
+	}
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+	}
 
+	onFileSelected(event: Event | any) {
+		if (event.target !== null || event.target.files.length === 0) {
+			this.updateBtnDisabled = false
+
+			const reader = new FileReader()
+			reader.readAsDataURL(event.target.files[0])
+			reader.onload = () => {
+				this.imgSrc = reader.result
+			}
+
+			this.inputFile = event.target.files[0]
+			console.log(this.inputFile)
+		}
+	}
+
+	updateLogo() {
+		if (this.inputFile) {
+			this.companyService.changeCompanyImage(this.inputFile).subscribe({
+				next: async (val) => {
+					this.snackbarService.open('Company logo updated')
+					this.localStorageService.setItem('c_img', val)
+					this.store.dispatch(setCompanyImage({companyImage: val}))
+					this.router.navigate(['/settings'])
+				},
+				error: () => {
+					this.snackbarService.open('Failed to update company logo')
+				}
+			})
+		}
+	}
 }
