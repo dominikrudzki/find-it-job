6b46a56a0d9ef884b6cfed6468edb382c17e5a9f
Dominik Rudzki <dominikrudzki2000@gmail.com>
2022-02-10 00:20:52 +0100

    create applications dialog component

---
+++ src/app/shared/dialogs/applications/applications.component.ts
@@ -0,0 +1,29 @@
+import { Component, Inject, OnInit } from '@angular/core'
+import { CompanyService } from "../../../core/services/company.service"
+import { MAT_DIALOG_DATA } from "@angular/material/dialog"
+import { SnackbarService } from "../../../core/services/snackbar.service"
+import { JobApplication } from "../../../core/interfaces/job-application"
+
+@Component({
+	selector: 'app-applications',
+	templateUrl: './applications.component.html',
+	styleUrls: ['./applications.component.scss']
+})
+export class ApplicationsComponent implements OnInit {
+	displayedColumns: string[] = ['name', 'applications', 'options']
+	userList: Array<JobApplication> = []
+
+	constructor(
+		private companyService: CompanyService,
+		@Inject(MAT_DIALOG_DATA) public data: { jobId: number },
+		private snackbarService: SnackbarService
+	) {
+	}
+
+	ngOnInit(): void {
+		this.companyService.getJobApplications(this.data.jobId).subscribe({
+			next: val => this.userList = val,
+			error: () => this.snackbarService.open('Unable to load applications')
+		})
+	}
+}
