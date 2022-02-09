69a3513c3ad82b5dc7d6d9c15670a7465714aadb
Dominik Rudzki <dominikrudzki2000@gmail.com>
2022-02-09 17:06:46 +0100

    add setting company name and image in ngrx state

--- src/app/shared/dialogs/login-dialog/login-dialog.component.ts
+++ src/app/shared/dialogs/login-dialog/login-dialog.component.ts
@@ -4,7 +4,13 @@ import { AuthService } from "../../../core/services/auth.service"
 import { FormControl, FormGroup, Validators } from "@angular/forms"
 import { SnackbarService } from "../../../core/services/snackbar.service"
 import { Store } from "@ngrx/store"
-import { setEmail, setIsEmployer, setIsLogged } from "../../../core/state/userInfo/userInfo.actions"
+import {
+	setCompanyImage,
+	setCompanyName,
+	setEmail,
+	setIsEmployer,
+	setIsLogged
+} from "../../../core/state/userInfo/userInfo.actions"
 import { Observable } from "rxjs"
 import { UserInfo } from "../../../core/interfaces/user-info"
 import { LocalStorageService } from "../../../core/services/local-storage.service"
@@ -84,12 +90,17 @@ export class LoginDialogComponent implements OnInit {
 				next: (value) => {
 					this.loginGroup.reset()
 					this.dialogRef.close()
+
 					const jwtPayload = this.authService.decodeAccessToken(value.accessToken)
+
 					this.store.dispatch(setIsLogged({isLogged: true}))
 					this.store.dispatch(setIsEmployer({isEmployer: jwtPayload?.employer!}))
 					this.store.dispatch(setEmail({email: jwtPayload?.email!}))
-					this.localStorageService.setItem('accessToken', value.accessToken)
-					this.localStorageService.setItem('refreshToken', value.refreshToken)
+					this.store.dispatch(setCompanyName({companyName: jwtPayload?.companyName!}))
+					this.store.dispatch(setCompanyImage({companyImage: jwtPayload?.companyImage!}))
+
+					this.localStorageService.setItem('access-token', value.accessToken)
+					this.localStorageService.setItem('refresh-token', value.refreshToken)
 				},
 				error: () => {
 					this.snackbar.open('Invalid credentials')
