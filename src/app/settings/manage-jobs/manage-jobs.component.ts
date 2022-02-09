b1b42def6592d7776163a05ec5b092a44a5d25ae
Dominik Rudzki <dominikrudzki2000@gmail.com>
2022-02-09 17:13:08 +0100

    add deleteJob and preview in manageJobs

--- src/app/settings/manage-jobs/manage-jobs.component.ts
+++ src/app/settings/manage-jobs/manage-jobs.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core'
+import { JobDataService } from "../../core/services/job-data.service"
+import { MatDialog } from "@angular/material/dialog"
+import { ConfirmDialogComponent } from "../../shared/dialogs/confirm-dialog/confirm-dialog.component"
+import { SnackbarService } from "../../core/services/snackbar.service"
+
+interface Job {
+	id: number,
+	name: string,
+	applications: number
+}
 
 @Component({
 	selector: 'app-manage-jobs',
@@ -7,23 +17,41 @@ import { Component, OnInit } from '@angular/core'
 })
 export class ManageJobsComponent implements OnInit {
 	displayedColumns: string[] = ['name', 'applications', 'options']
-	dataSource = [
-		{name: 'Hydrogen', applications: 1.0079, id: 'H'},
-		{name: 'Helium', applications: 4.0026, id: 'He'},
-		{name: 'Lithium', applications: 6.941, id: 'Li'},
-		{name: 'Beryllium', applications: 9.0122, id: 'Be'},
-		{name: 'Boron', applications: 10.811, id: 'B'},
-		{name: 'Carbon', applications: 12.0107, id: 'C'},
-		{name: 'Nitrogen', applications: 14.0067, id: 'N'},
-		{name: 'Oxygen', applications: 15.9994, id: 'O'},
-		{name: 'Fluorine', applications: 18.9984, id: 'F'},
-		{name: 'Neon', applications: 20.1797, id: 'Ne'}
-	]
+	jobList: Array<Job> = []
 
-	constructor() {
+	constructor(
+		private jobDataService: JobDataService,
+		public dialog: MatDialog,
+		private snackbarService: SnackbarService
+	) {
 	}
 
 	ngOnInit(): void {
+		this.jobDataService.getEmployerJobs().subscribe({
+			next: (val) => {
+				console.log(val)
+				this.jobList = val
+			},
+			error: err => {
+				console.log(err)
+			}
+		})
 	}
 
+	deleteJob(id: number): void {
+		const dialogRef = this.dialog.open(ConfirmDialogComponent, {restoreFocus: false})
+		dialogRef.afterClosed().subscribe((res: boolean) => {
+			if (res) {
+				this.jobDataService.deleteJob(id).subscribe({
+					next: () => {
+						this.jobList = this.jobList.filter(job => job.id !== id)
+						this.snackbarService.open('Job deleted')
+					},
+					error: () => {
+						this.snackbarService.open('Unable to delete job')
+					}
+				})
+			}
+		})
+	}
 }
