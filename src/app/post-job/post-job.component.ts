6926961baef45ffdcbcf85f3d286209648d25bb1
Dominik Rudzki <dominikrudzki2000@gmail.com>
2022-02-09 16:58:57 +0100

    set validators to post job component

--- src/app/post-job/post-job.component.ts
+++ src/app/post-job/post-job.component.ts
@@ -2,13 +2,14 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core'
 import { ENTER } from "@angular/cdk/keycodes"
 import { MatChipInputEvent } from "@angular/material/chips"
 import { map, Observable, startWith } from "rxjs"
-import { FormControl } from "@angular/forms"
+import { FormControl, FormGroup, Validators } from "@angular/forms"
 import { environment } from "../../environments/environment"
 import { JobDataService } from "../core/services/job-data.service"
+import { SnackbarService } from "../core/services/snackbar.service"
 
 interface Item {
 	name: string,
-	level?: number
+	level: number
 }
 
 @Component({
@@ -18,18 +19,34 @@ interface Item {
 })
 export class PostJobComponent implements OnInit {
 	readonly separatorKeysCodes = [ENTER] as const
-	benefits: Array<Item> = [{name: 'Macbook Pro'}, {name: '2 monitors'}]
-	skills: Array<Item> = [{name: 'Angular', level: 4}, {name: 'React', level: 5}]
+	selectOption: string = "0"
+	benefits: Array<string> = []
+	skills: Array<Item> = []
+
+	addJobForm: FormGroup = new FormGroup({
+		name: new FormControl(
+			'',
+			[Validators.required,
+				Validators.minLength(2),
+				Validators.maxLength(32)
+			]),
+		remote: new FormControl('', [Validators.required]),
+		salaryMin: new FormControl('', [Validators.required]),
+		salaryMax: new FormControl('', [Validators.required]),
+		description: new FormControl('', [Validators.required]),
+		experience: new FormControl('', [Validators.required])
+	})
 
 	myControl = new FormControl()
 	options: string[] = environment.technologies
 	filteredOptions: Observable<string[]>
 
-	selectOption: number = 1
 	@ViewChild('skill') skillInput!: ElementRef
-	@ViewChild('exp') expOption!: ElementRef
 
-	constructor(private jobDataService: JobDataService) {
+	constructor(
+		private jobDataService: JobDataService,
+		private snackbarService: SnackbarService
+	) {
 		this.filteredOptions = this.myControl.valueChanges.pipe(
 			startWith(''),
 			map(value => this._filter(value))
@@ -47,35 +64,58 @@ export class PostJobComponent implements OnInit {
 	addBenefit(event: MatChipInputEvent): void {
 		const value = (event.value || '').trim()
 		if (value) {
-			this.benefits.push({name: value})
+			this.benefits.push(value)
 		}
 		event.chipInput!.clear()
 	}
 
 	addSkill(name: string, level: number) {
-		if (name && level) {
+		level = parseInt(String(level))
+		if (name && (level !== 0)) {
 			this.skills.push({name, level})
 			this.skillInput.nativeElement.value = ''
-			this.selectOption = 0
+			this.selectOption = "0"
 		}
 	}
 
-	remove(array: Array<Item>, item: Item): void {
+	remove(array: Array<Item | string>, item: Item | string): void {
 		const index = array.indexOf(item)
 		if (index >= 0) {
 			array.splice(index, 1)
 		}
 	}
 
-
-	postJob() {
-		this.jobDataService.addJob().subscribe({
-			next: (val) => {
-
-			},
-			error: (err) => {
-
-			}
-		})
+	submitFrom() {
+		if (this.addJobForm.valid) {
+			this.jobDataService.addJob(
+				{
+					name: this.addJobForm.get('name')?.value,
+					remote: this.addJobForm.get('remote')?.value === "true",
+					benefits: this.benefits,
+					company: "", // TODO: get your company from store
+					description: this.addJobForm.get('description')?.value,
+					experience: this.addJobForm.get('experience')?.value,
+					salary: {
+						min: this.addJobForm.get('salaryMin')?.value,
+						max: this.addJobForm.get('salaryMax')?.value
+					},
+					skills: this.skills
+				}
+			).subscribe({
+				next: () => {
+					this.addJobForm.reset()
+					this.benefits = []
+					this.skills = []
+					this.snackbarService.open('Job added')
+				},
+				error: (err) => {
+					this.snackbarService.open('Failed to add a job')
+					console.log(err)
+				}
+			})
+		} else {
+			this.snackbarService.open('Form is not valid')
+		}
 	}
+
 }
