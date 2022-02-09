c5900d7377e3a9a950b862559b95bc4454dba060
Dominik Rudzki <dominikrudzki2000@gmail.com>
2022-02-10 00:27:20 +0100

    add getToken with interval to silent jwt refresh

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,8 +10,6 @@ import {
 	setIsLogged
 } from "./core/state/userInfo/userInfo.actions"
 import { AuthService } from './core/services/auth.service'
-import { take } from "rxjs"
-
 
 @Component({
 	selector: 'app-root',
@@ -24,43 +22,48 @@ export class AppComponent {
 		private store: Store<{ userInfo: UserInfo }>,
 		private authService: AuthService
 	) {
-		const body = document.body.classList // TODO: dark mode (handle in store)
-		body.add('darkTheme')
+		const accessToken = localStorageService.getItem('access-token')
 
-		if (localStorageService.getItem('refresh-token')) {
+		if (accessToken) {
+			const jwtPayload = this.authService.decodeAccessToken(accessToken)
+
 			this.store.dispatch(setIsLogged({isLogged: true}))
+			this.store.dispatch(setIsEmployer({isEmployer: jwtPayload?.employer!}))
 
-			this.authService.refreshToken().pipe(take(1)).subscribe({
-				next: ({accessToken}) => {
-					this.localStorageService.setItem('access-token', accessToken)
-
-					const jwtPayload = authService.decodeAccessToken(accessToken)
-
-					this.store.dispatch(setEmail({
-						email: jwtPayload?.email!
-					}))
-
-					this.store.dispatch(setIsEmployer({
-						isEmployer: jwtPayload?.employer!
-					}))
-
-					this.store.dispatch(setCompanyName({
-						companyName: jwtPayload?.companyName!
-					}))
-
-					this.store.dispatch(setCompanyImage({
-						companyImage: jwtPayload?.companyImage!
-					}))
-					// setInterval(() => {
-					// // TODO: silent access-token refresh
-					// }, 1000 * 60 * 29) // 29min
-				},
-				error: (err) => {
-					console.log(err)
-					this.localStorageService.removeItem('access-token')
-					this.localStorageService.removeItem('refresh-token')
-				}
-			})
+			this.getToken()
+			setInterval(this.getToken, 1000 * 60 * 29) // 29min
 		}
+	}
+
+	private getToken() {
+		this.authService.refreshToken().subscribe({
+			next: ({accessToken}) => {
+				const jwtPayload = this.authService.decodeAccessToken(accessToken)
+
+				this.localStorageService.setItem('access-token', accessToken)
+
+				this.store.dispatch(setEmail({
+					email: jwtPayload?.email!
+				}))
+
+				this.store.dispatch(setIsEmployer({
+					isEmployer: jwtPayload?.employer!
+				}))
+
+				this.store.dispatch(setCompanyName({
+					companyName: jwtPayload?.companyName!
+				}))
+
+				this.store.dispatch(setCompanyImage({
+					companyImage: jwtPayload?.companyImage!
+				}))
+
+			},
+			error: (err) => {
+				console.log(err)
+				this.localStorageService.removeItem('access-token')
+				this.localStorageService.removeItem('refresh-token')
+			}
+		})
 	}
 }
