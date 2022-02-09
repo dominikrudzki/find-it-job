6753f0d501fcf56c26d827a5fa4b21cf5f344f65
Dominik Rudzki <dominikrudzki2000@gmail.com>
2022-02-09 16:55:44 +0100

    add company logo display in settings

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { Store } from "@ngrx/store"
+import { UserInfo } from "../core/interfaces/user-info"
+import { Observable } from "rxjs"
+import { environment } from "../../environments/environment"
 
 @Component({
-  selector: 'app-settings',
-  templateUrl: './settings.component.html',
-  styleUrls: ['./settings.component.scss']
+	selector: 'app-settings',
+	templateUrl: './settings.component.html',
+	styleUrls: ['./settings.component.scss']
 })
 export class SettingsComponent implements OnInit {
+	$companyName: Observable<string | undefined> = this.store.select((state) => state.userInfo.companyName)
+	$companyImage: Observable<string | undefined> = this.store.select((state) => environment.imageUrl + state.userInfo.companyImage)
 
-  constructor() { }
+	constructor(private store: Store<{ userInfo: UserInfo }>) {
+	}
 
-  ngOnInit(): void {
-  }
-
+	ngOnInit(): void {
+	}
 }
