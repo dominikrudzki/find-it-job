e2d305ce3239d76ffa540c4d805139c2d831b752
Dominik Rudzki <dominikrudzki2000@gmail.com>
2022-02-09 17:00:36 +0100

    add deleteJob & getEmployerJobs methods

--- src/app/core/services/job-data.service.ts
+++ src/app/core/services/job-data.service.ts
@@ -1,15 +1,19 @@
 import { Injectable } from '@angular/core'
-import { HttpClient } from "@angular/common/http"
+import { HttpClient, HttpHeaders } from "@angular/common/http"
 import { JobDetails } from "../interfaces/job-details"
 import { Observable } from "rxjs"
 import { environment } from "../../../environments/environment"
+import { LocalStorageService } from "./local-storage.service"
 
 @Injectable({
 	providedIn: 'root'
 })
 export class JobDataService {
 
-	constructor(private http: HttpClient) {
+	constructor(
+		private http: HttpClient,
+		private localStorageService: LocalStorageService
+	) {
 	}
 
 	getJob(id: number): Observable<JobDetails> {
@@ -20,12 +24,39 @@ export class JobDataService {
 		return this.http.get<any>(`${environment.apiUrl}/get-jobs`)
 	}
 
-	addJob() {
+	getEmployerJobs() {
+		console.log(this.localStorageService.getItem('access-token'))
+		return this.http.get<any>(
+			`${environment.apiUrl}/get-company-jobs`,
+			{
+				headers: new HttpHeaders(
+					{'Authorization': 'Bearer ' + this.localStorageService.getItem('access-token')}
+				)
+			}
+		)
+	}
+
+	addJob(jobData: JobDetails) {
 		return this.http.put(
 			`${environment.apiUrl}/add-job`,
-			{},
-			{}
-			// TODO add job request
+			jobData,
+			{
+				headers: new HttpHeaders(
+					{'Authorization': 'Bearer ' + this.localStorageService.getItem('access-token')}
+				)
+			}
+		)
+	}
+
+	deleteJob(jobId: number) {
+		return this.http.delete(
+			`${environment.apiUrl}/delete-job`,
+			{
+				body: {jobId},
+				headers: new HttpHeaders(
+					{'Authorization': 'Bearer ' + this.localStorageService.getItem('access-token')}
+				)
+			}
 		)
 	}
 }
