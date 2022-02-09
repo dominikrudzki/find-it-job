c6c0a2c0a0ee869aa7bdf2e70ae140b092404d3a
Dominik Rudzki <dominikrudzki2000@gmail.com>
2022-02-09 16:52:51 +0100

    add refreshToken method to auth service

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,18 +1,22 @@
 import { Injectable } from '@angular/core'
-import { HttpClient } from "@angular/common/http"
+import { HttpClient, HttpHeaders } from "@angular/common/http"
 import { Observable } from "rxjs"
 import { environment } from "../../../environments/environment"
 import { RegisterData } from "../interfaces/register-data"
 import { LoginData } from "../interfaces/login-data"
 import jwt_decode from 'jwt-decode'
 import { jwtPayload } from "../interfaces/jwt-payload"
+import { LocalStorageService } from "./local-storage.service"
 
 @Injectable({
 	providedIn: 'root'
 })
 export class AuthService {
 
-	constructor(private http: HttpClient) {
+	constructor(
+		private http: HttpClient,
+		private localStorageService: LocalStorageService
+	) {
 	}
 
 	register(data: RegisterData): Observable<any> {
@@ -37,5 +41,17 @@ export class AuthService {
 		} catch (err) {
 			return null
 		}
+	}
+
+	refreshToken(): Observable<any> {
+		return this.http.post(
+			`${environment.apiUrl}/refresh-token`,
+			{},
+			{
+				headers: new HttpHeaders(
+					{'Authorization': 'Bearer ' + this.localStorageService.getItem('refresh-token')}
+				)
+			}
+		)
 	}
 }
