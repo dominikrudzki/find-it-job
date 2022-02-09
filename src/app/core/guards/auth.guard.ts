7958eb87f5f11f8c11475a768be9a710a0679564
Dominik Rudzki <dominikrudzki2000@gmail.com>
2022-02-10 00:28:22 +0100

    remove canActivateChild in auth guard

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -2,19 +2,18 @@ import { Injectable } from '@angular/core'
 import {
 	ActivatedRouteSnapshot,
 	CanActivate,
-	CanActivateChild,
 	Router,
 	RouterStateSnapshot,
 	UrlTree
 } from '@angular/router'
-import { Observable, take } from 'rxjs'
+import { Observable } from 'rxjs'
 import { Store } from "@ngrx/store"
 import { UserInfo } from "../interfaces/user-info"
 
 @Injectable({
 	providedIn: 'root'
 })
-export class AuthGuard implements CanActivate, CanActivateChild {
+export class AuthGuard implements CanActivate {
 	constructor(
 		private router: Router,
 		private readonly store: Store<{ userInfo: UserInfo }>
@@ -26,21 +25,13 @@ export class AuthGuard implements CanActivate, CanActivateChild {
 		state: RouterStateSnapshot):
 		Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree | any {
 		let isLogged: boolean = false
-		this.store.select(state => state.userInfo.isLogged).pipe(take(1)).subscribe(
+		this.store.select(state => state.userInfo.isLogged).subscribe(
 			(logged: boolean) => isLogged = logged
 		)
 		if (isLogged) {
 			return true
-		} else {
-			this.router.navigate(['/'])
-			return false
 		}
+		this.router.navigate(['/'])
+		return false
 	}
-
-	canActivateChild(
-		childRoute: ActivatedRouteSnapshot,
-		state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-		return true
-	}
-
 }
