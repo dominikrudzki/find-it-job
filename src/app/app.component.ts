e9086598de0b1d36c735746d3413532684a7b495
Dominik Rudzki <dominikrudzki2000@gmail.com>
2022-02-09 16:52:20 +0100

    add reading jwtPayload and setting state

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,14 @@ import { LocalStorageService } from './core/services/local-storage.service'
 import { Store } from "@ngrx/store"
 import { UserInfo } from "./core/interfaces/user-info"
 import {
+	setCompanyImage,
 	setCompanyName,
 	setEmail,
 	setIsEmployer,
 	setIsLogged
 } from "./core/state/userInfo/userInfo.actions"
 import { AuthService } from './core/services/auth.service'
+import { take } from "rxjs"
 
 
 @Component({
@@ -25,22 +27,40 @@ export class AppComponent {
 		const body = document.body.classList // TODO: dark mode (handle in store)
 		body.add('darkTheme')
 
-		if (localStorageService.getItem('refreshToken')) {
+		if (localStorageService.getItem('refresh-token')) {
 			this.store.dispatch(setIsLogged({isLogged: true}))
 
-			const jwtPayload = authService.decodeAccessToken(localStorageService.getItem('accessToken')!)
+			this.authService.refreshToken().pipe(take(1)).subscribe({
+				next: ({accessToken}) => {
+					this.localStorageService.setItem('access-token', accessToken)
 
-			this.store.dispatch(setEmail({
-				email: jwtPayload?.email!
-			}))
+					const jwtPayload = authService.decodeAccessToken(accessToken)
 
-			this.store.dispatch(setIsEmployer({
-				isEmployer: jwtPayload?.employer!
-			}))
+					this.store.dispatch(setEmail({
+						email: jwtPayload?.email!
+					}))
 
-			this.store.dispatch(setCompanyName({
-				companyName: jwtPayload?.companyName!
-			}))
+					this.store.dispatch(setIsEmployer({
+						isEmployer: jwtPayload?.employer!
+					}))
+
+					this.store.dispatch(setCompanyName({
+						companyName: jwtPayload?.companyName!
+					}))
+
+					this.store.dispatch(setCompanyImage({
+						companyImage: jwtPayload?.companyImage!
+					}))
+					// setInterval(() => {
+					// // TODO: silent access-token refresh
+					// }, 1000 * 60 * 29) // 29min
+				},
+				error: (err) => {
+					console.log(err)
+					this.localStorageService.removeItem('access-token')
+					this.localStorageService.removeItem('refresh-token')
+				}
+			})
 		}
 	}
 }
