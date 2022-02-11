7f8690ec1cf7d91530c9633582d7a32f909e4230
Dominik Rudzki <dominikrudzki2000@gmail.com>
2022-02-11 23:01:18 +0100

    create user-applications component

--- src/app/settings/user-applications/user-applications.component.ts
+++ src/app/settings/user-applications/user-applications.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core'
+import { DeveloperService } from "../../core/services/developer.service"
+import { SnackbarService } from "../../core/services/snackbar.service"
 
 @Component({
 	selector: 'app-user-applications',
@@ -6,13 +8,28 @@ import { Component, OnInit } from '@angular/core'
 	styleUrls: ['./user-applications.component.scss']
 })
 export class UserApplicationsComponent implements OnInit {
+	displayedColumns: string[] = ['name', 'options']
+	applicationList: Array<{ id: number, name: string }> = [
+		{id: 1, name: 'template job'}
+	]
 
-	constructor() {
+	constructor(
+		private developerService: DeveloperService,
+		private snackbarService: SnackbarService
+	) {
 	}
 
 	ngOnInit(): void {
+		this.developerService.getUserApplications().subscribe({
+			next: val => this.applicationList = val,
+			error: () => this.snackbarService.open('Server error')
+		})
 	}
 
-	changePassword() {
+	deleteApplication(id: number) {
+		this.developerService.deleteUserApplication(id).subscribe({
+			next: () => this.applicationList = this.applicationList.filter(value => value.id !== id),
+			error: () => this.snackbarService.open('Server error')
+		})
 	}
 }
