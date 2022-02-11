e5cf3a7dfb9a0d5e31c5d2e2a88ea14a5be340b0
Dominik Rudzki <dominikrudzki2000@gmail.com>
2022-02-11 23:03:00 +0100

    update apply button logic

--- src/app/job-preview/job-preview.component.ts
+++ src/app/job-preview/job-preview.component.ts
@@ -3,6 +3,8 @@ import { JobDetails } from "../core/interfaces/job-details"
 import { JobDataService } from '../core/services/job-data.service'
 import { ActivatedRoute } from "@angular/router"
 import { environment } from "../../environments/environment"
+import { DeveloperService } from "../core/services/developer.service"
+import { SnackbarService } from "../core/services/snackbar.service"
 
 @Component({
 	selector: 'app-job-preview',
@@ -12,19 +14,37 @@ import { environment } from "../../environments/environment"
 export class JobPreviewComponent implements OnInit {
 	job?: JobDetails
 	jobImageUrl = environment.imageUrl
+	applied: boolean = false
 
 	jobDataSub = this.jobData.getJob(Number(this.route.snapshot.paramMap.get('jobId'))).subscribe(val => {
 		this.job = val
 	})
 
-	constructor(private jobData: JobDataService, private route: ActivatedRoute) {
+	constructor(
+		private jobData: JobDataService,
+		private route: ActivatedRoute,
+		private developerService: DeveloperService,
+		private snackbarService: SnackbarService
+	) {
 	}
 
 	ngOnInit(): void {
 	}
 
 	apply() {
-		//	TODO: apply using this job.id
+		this.developerService.applyForJob(this.job?.id!).subscribe({
+			next: () => {
+				this.snackbarService.open('You applied this job')
+				this.applied = true
+			},
+			error: (err) => {
+				if (err.status = 401) {
+					// TODO: open login modal
+				}
+
+				this.snackbarService.open('Server error')
+			}
+		})
 	}
 
 	ngOnDestroy() {
