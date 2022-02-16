5c46402c5655cb890190f11536cd7a83afb6c260
Dominik Rudzki <dominikrudzki2000@gmail.com>
2022-02-16 23:22:31 +0100

    create paginator component

---
+++ src/app/jobs-page/paginator/paginator.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core'
+import { Router } from "@angular/router"
+
+@Component({
+	selector: 'app-paginator',
+	templateUrl: './paginator.component.html',
+	styleUrls: ['./paginator.component.scss']
+})
+export class PaginatorComponent implements OnInit {
+
+	constructor(private router: Router) {
+	}
+
+	ngOnInit(): void {
+	}
+
+	onclick() {
+		this.router.navigate(['/'], {queryParams: {type: 'firstRegister'}})
+		console.log('ggg')
+	}
+}
