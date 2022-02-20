ed6d594447cf0becff327b687b5b4019ddae1355
Dominik Rudzki <dominikrudzki2000@gmail.com>
2022-02-20 22:55:19 +0100

    update JobDetails interface

--- src/app/core/interfaces/job-details.ts
+++ src/app/core/interfaces/job-details.ts
@@ -2,7 +2,7 @@ export interface JobDetails {
 	id?: number;
 	name: string;
 	image?: string;
-	company: string;
+	company?: string;
 	remote: boolean;
 	salary?: { min: number, max: number };
 	description?: string;
